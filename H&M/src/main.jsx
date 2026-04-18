4df27d96c480a9bd82900011e13b9969a041f1d0
User <user@example.com>
2026-04-19 04:09:23 +0900

    H&M: Launching the high-fashion React clone with premium styling

--- H&M/src/main.jsx
+++ H&M/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/css--react/hm">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
